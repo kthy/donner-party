1502c0a986d57436c4e239654c0e0c5c2b9446be
Kristian Thy <thy@42.dk>
2020-07-30 23:37:08 +0200

    fix: delete old CSVs before loading

--- db_load_data.cmd
+++ db_load_data.cmd
@@ -5,6 +5,7 @@ SET datadir="C:\Users\thy\source\repos\donner-graph"
 SET neodir="C:\Users\thy\.Neo4jDesktop\neo4jDatabases\database-18ded114-d1bb-42db-95e6-e67282017fb1\installation-4.1.0"
 SET neopwd=UZFbY3QIVYeue5uuboDJ
 
+DEL /Q %neodir%\import\*.csv
 XCOPY /Q /Y %datadir%\nodes\*.csv %neodir%\import\
 XCOPY /Q /Y %datadir%\relations\*.csv %neodir%\import\
 TYPE %datadir%\party.cypher | %neodir%\bin\cypher-shell -u neo4j -p %neopwd% --format verbose --debug
